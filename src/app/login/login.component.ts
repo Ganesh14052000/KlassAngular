fadc9c1c6c5b337e4181bfb863dfb37af905680c
Ganesh Revanna <ganeshrevanna2000@gmail.com>
2025-01-26 16:11:01 +0530

    Login Form Modifications

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,32 @@
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [ MatCardModule, 
-    ReactiveFormsModule,   
-    FormsModule,    // For <mat-card>
-    MatFormFieldModule,   // For <mat-form-field>
+  imports: [ MatCardModule,     // For <mat-card>
+    MatFormFieldModule, 
+    FormsModule , 
+    ReactiveFormsModule,   // For <mat-form-field>
     MatInputModule,       // For <mat-input>
     MatButtonModule,      // For <mat-button>
     MatIconModule, 
-  MatSnackBarModule],
+  MatSnackBarModule,
+],
+  
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private snackBar: MatSnackBar){}
-  Login(){
-    this.snackBar.open('SuccessFully Logged in');
-    console.log('Button Working')
+  username: string = '';
+  password: string = '';
+  @ViewChild('myForm') myForm: NgForm | undefined;
+  constructor(){}
+  Login(value:any){
+   console.log('Form Values',this.myForm?.value)
   }
 }
